292fec85c4906b51457ff1a78a463eef01c7611f
Alex <sanches81@list.ru>
2022-10-26 16:17:36 +0400

    Rename varibale in dka_price.py

--- dka_price.py
+++ dka_price.py
@@ -13,24 +13,23 @@ load_dotenv(find_dotenv())
 
 customer_id = sys.argv[1]
 reciver = sys.argv[2]
-now = date.today()
+date_today = date.today()
 username = os.getenv('USERNAME_REPORT_SERVER')
 password = os.getenv('PASSWORD_REPORT_SERVER')
 sender = os.getenv('SENDER')
 alias_sender = os.getenv('SENDER_ALIAS')
 token = os.getenv('TOKEN_GMAIL')
 
-query = f'''http://192.168.1.104/ReportServer/Pages/ReportViewer.aspx?%2fDynamicsAX%2fCustPriceList.PrecisionDesign&CustPriceList_CustAccount={customer_id}&CustPriceList_PerDate={now}&CustPriceList_CurrencyCode=usd&CustPriceList_DataArea=dka&rs:Command=Render&rs:Format=Excel''' # pylint: disable=line-too-long
+query = f'''http://192.168.1.104/ReportServer/Pages/ReportViewer.aspx?%2fDynamicsAX%2fCustPriceList.PrecisionDesign&CustPriceList_CustAccount={customer_id}&CustPriceList_PerDate={date_today}&CustPriceList_CurrencyCode=usd&CustPriceList_DataArea=dka&rs:Command=Render&rs:Format=Excel''' # pylint: disable=line-too-long
 
 def main():
     """
     Главная функция
     """
-    # exctraction(customer_id,now,username,password)
-    formating(exctraction(query,username,password),customer_id,now)
-    file_excel = f'Price_list-{customer_id}-{now}.xlsx'
+    # exctraction(customer_id,date_today,username,password)
+    formating(exctraction(query,username,password),customer_id,date_today)
+    file_excel = f'Price_list-{customer_id}-{date_today}.xlsx'
     send_email(sender, reciver, token, alias_sender, file_excel)
-
 
 if __name__ == "__main__":
     print("Запуск приложения ", main())
